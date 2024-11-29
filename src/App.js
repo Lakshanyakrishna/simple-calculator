1a365ecf531ac96dc19318ec6a086bb98cfdb1ef
Lakshanyakrishna <146199117+Lakshanyakrishna@users.noreply.github.com>
2024-11-29 14:48:47 +0530

    the rest
    public and source

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculator from './Components/Calculator';
+ 
+ 
+function App() {
+  return (
+     <>
+     <h1 class="centered-title">Quick Math Calculator</h1>
+   <Calculator/>
+     </>
+  );
+}
+
+export default App;
